bcca968888c65c5bdbadbd51529d075a00e79b36
ncdanie <nathancdaniels@gmail.com>
2023-04-07 17:03:48 -0700

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import * as ReactDOM from 'react-dom/client'
+import React from 'react'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <div>
+      <h1>React App</h1>
+    </div>
+  </React.StrictMode>
+)
